50540f013678e6079a3ca36060a5f20861bb932f
Unknown <morgan.fraisse@hotmail.fr>
2017-12-22 11:56:53 +0100

    footer balises 2

--- cgv.php
+++ cgv.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Swapp - Conditions générales de vente</title>
 </head>
 <body>
