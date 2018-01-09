00da7c1bbb8f93e2bfdb4932bad12afb44f7c5e0
Unknown <morgan.fraisse@hotmail.fr>
2018-01-09 12:27:39 +0100

    staged files!

--- vue/jobs.php
+++ vue/jobs.php
@@ -20,6 +20,6 @@
 <div><?php include('../includes/footer.php'); ?></div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap.min.js"></script>
+    <script src="../JS/bootstrap.min.js"></script>
 </body>
 </html>
