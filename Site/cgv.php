249c48171d75398c1f70a27f89be7fdd582e7ea0
Unknown <hmscompagnie@gmail.com>
2018-01-09 09:30:03 +0100

    Cgv EMMA

--- Site/cgv.php
+++ Site/cgv.php
@@ -1,24 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="bootstrap.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Swapp - Conditions générales de vente</title>
-</head>
-<body>
-<div><?php include('includes/banner.php'); ?></div>
-<h1 class="centrer">Conditions générales de vente</h1>
-<section>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos tempore officia tenetur cum quis maxime, illo officiis dolorum quasi quia adipisci eos blanditiis commodi libero. Sint ullam cupiditate amet error at perspiciatis quasi animi modi impedit earum voluptas dolor ipsam, quis, reiciendis dignissimos eum aperiam fuga rem voluptatibus quos dicta maxime. Est veniam harum, qui iste nisi vel placeat sequi? Dolore nihil similique quibusdam ratione nemo, sequi molestiae accusamus blanditiis illo explicabo non magnam magni perferendis quis doloremque quisquam quae consequuntur deserunt cum veritatis eveniet impedit. Architecto aperiam nemo esse, culpa amet sunt adipisci illo officiis ipsam non quam quas.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugit placeat iste doloremque cumque. Iste corrupti harum earum provident ab cupiditate consequatur possimus labore voluptate, minus, quasi placeat id ullam aliquam explicabo eaque cum! Quae doloremque voluptate nulla natus praesentium quisquam magnam eius assumenda rerum voluptas, similique, quibusdam voluptatem reiciendis architecto! Odio laboriosam totam harum incidunt blanditiis dicta ullam dolores architecto rem beatae eos autem id quidem, sed quia consequuntur reiciendis nesciunt, eveniet, placeat fugiat? Recusandae eum nulla hic provident sequi esse totam vero, iure pariatur illum incidunt unde adipisci fugit sed distinctio perferendis omnis modi. Dolores rem dolore tempora sed eos et? Voluptas, quibusdam facilis a molestias aliquid maxime eius nihil blanditiis nam omnis cupiditate vel vitae commodi adipisci possimus animi debitis maiores sunt itaque voluptatem consectetur vero tenetur beatae! Maxime placeat quaerat eius quidem eos vitae dicta, labore ex earum commodi libero dolorem impedit veritatis consectetur debitis est suscipit itaque vero tenetur cupiditate. Ea dolorum vel harum, vitae maxime sunt impedit error in hic fuga quod nisi cumque distinctio. Optio, ab iste cum quod laboriosam impedit recusandae cupiditate necessitatibus! Nulla suscipit possimus minus, neque quia maxime sint eveniet. Molestiae dignissimos error accusamus atque non molestias ipsam numquam temporibus.</p>
-</section>
-
-
-<div><?php include('includes/footer.php'); ?></div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap.min.js"></script>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Swapp - Conditions générales de vente</title>
+</head>
+<body>
+<div><?php include('includes/banner.php'); ?></div>
+<h1 class="centrer">Conditions générales de vente</h1>
+<section>
+        <h2>Préambule</h2><br>
+            Swappist est une plateforme de troc permettant à ses membres d'effectuer des échanges non monétaires, avec notre monnaie locale virtuelle, le <b>SouApp</b>.
+
+            Swappist a mis en place une vente d'objets promotionnels (tee-shirts, fers à repasser, sanibroyeur, etc...) afin de financer le fonctionnement de la plateforme.
+
+            Les présentes Conditions Générales de Vente (CGV) ont pour objet l'encadrement juridique de la vente de ces objets promotionnels.
+            Toute connexion au site est subordonnée au respect des présentes conditions. Pour l'utilisateur, le simple accès au site de Swappist - <a href="www.swappist.html">www.swappist.fr</a> - implique l'acceptation expresse et irrévocable de l'ensemble des conditions décrites ci-après.
+            Swappist se réserve le droit de modifier unilatéralement et à tout moment le contenu des présentes conditions générales d'utilisation. Le membre devra alors à sa prochaine connexion valider les nouvelles CGV.
+            L'accès à ce service est également subordonné au respect des Conditions Générales d'Utilisation. Tout utilisateur souhaitant y accéder doit avoir pris connaissance préalablement de ces Conditions Générales d'Utilisation et s'engage à les respecter sans réserve.
+            En procédant à une commande sur le site, vous déclarez sur l'honneur avoir la capacité juridique d'effectuer un achat.
+        <h2>Définitions</h2><br>
+
+    <b>Carte Bancaire :</b><br>
+            Carte de paiement ou de crédit, en cours de validité et valable en France, sous réserve de leur acceptation par le système <b>E-Transaction</b>.<br>
+    <br><b>E-transactions :</b><br>
+            Désigne le service sécurisé de paiement bancaire du <b>Crédit Agricole</b> (sponsorisé par nos amis anonymes, Valérie C. & Morgan F. dont nous tairons les noms.<br>
+    <br><b>Ticket paiement :</b><br>
+            Désigne la facture attestant la garantie du paiement.<br>
+    <br><b>Remboursement :</b><br>
+            Remboursement par <b>Swappist</b> d'une transaction bancaire d'achat par un Utilisateur, diminuée le cas échéant des frais applicables, et dans la limite de la réglementation applicable.<br>
+    <br><b>Transaction :</b><br>
+            Transaction conclue par un Utilisateur dans les conditions visées aux présentes CGV, dans le respect des CGU et donnant lieu à une Opération de paiement lors d'un achat.
+
+        <h2>Modalités</h2>
+        Dans le cadre de l'achat d'objets promotionnels par un utilisateur, Swappist fait appel au service e-transaction du Crédit Agricole. L'achat d'objets promotionnels n'est pas limité en quantité. Les prix indiqués sur le site sont en Euros.
+            Il n y a aucune obligation d'achat pour pouvoir bénéficier des services offerts par la plateforme Swappist.
+
+            <h3>Participation libre</h3>
+        Une participation libre peut être ajoutée à la commande, cette participation est soumise à la TVA en vigueur. Comme son nom l'indique la participation libre n'a aucun caractère obligatoire et ne donne droit à aucune contrepartie spécifique.
+
+        <h3>Délai de livraison</h3>
+        La ventes des objets promotionnels s'effectue en prévente, les objets promotionnels sont fabriqués; lorsque qu'un volume de commande global (pour chaque objet) est atteint, les délais de livraison peuvent varier.
+        Les seuils de mise en production des objets promotionnels sont fixé à 50 unités. Si le seuil de fabrication n'est pas atteint et qu'une commande est en attente depuis plus de 4 semaines Swappist se reserve le droit d'annuler et de rembourser la commande, ou de procéder à la réalisation, ou de reconduire un nouveau délai de 4 semaines supplémentaires jusqu'aggrave; atteinte des seuils de réalisations.
+
+        <h3>Modalités de paiement</h3>
+        Le règlement des achats s'effectue au comptant par carte bancaire par l'intermédiaire du service E-transaction. Le montant en numéraire, exprimé en euros versé par l'utilisateur est collecté par E-transaction.
+
+        <h3>Délai de rétractation et Remboursement</h3>
+        L'utilisateur peut demander l'annulation d'une transaction sous 14 jours sur le site Swappist.
+        Pour que la demande de remboursement soit enregistrée, l'Utilisateur doit adresser celle-ci en nous envoyant <a href="mailto:michel.drucker@bogoss.com">un e-mail à cette adresse</a>.
+        L'Utilisateur doit alors indiquer les références de l'achat qu'il souhaite voir rembourser (numero de facture, numero de commande). Swappist initie un ordre de crédit sur la carte bancaire dans un délai de 15 jours ouvrés à compter de la réception de la demande de l'Utilisateur.
+        l'Utilisateur est tenu de communiquer toutes pièces justificatives demandées par Swappist le cas échéant.
+
+        <h3>Contestation d'une Opération de paiement</h3>
+    Pour toute réclamation relative aux opérations réalisées par Swappist dans le cadre des présentes CGV, telles qu'une Opération de paiement ou toute opération de débit, de crédit du compte de l'Utilisateur au titre notamment de l'achat, de la vente ou d'un remboursement, l'Utilisateur doit s'adresser au service client de Swappist en écrivant à <a href=mailto:"michel.drucker@meetic.com">cette adresse e-mail</a>.
+        L'Utilisateur qui désire contester une Opération de paiement qu'il estime non autorisée, mal exécutée ou frauduleuse doit transmettre sa demande à Swappist dans les meilleurs délais et avant l'expiration d'un délai de quartoze (14) jours à compter de la date de l'Opération de paiement en question.
+        Swappist ne saurait voir engager sa responsabilité en cas de faute de l'Utilisateur, de manquement intentionnel ou d'une négligence fautive à ses obligations, de transmission tardive d'une demande d'opposition ou d'une contestation de mauvaise foi.
+
+        <h3>Facturation et Rapport d'activité</h3>
+        Swappist envoie une facture numérique adressée sur l'e-mail de l'acheteur. un Ticket paiement sera également adressé sur l'e-mail de l'acheteur par le service E-Transactions.
+        Swappist conservera un descriptif complet des achats d'objets promotionnels réalisés par les Utilisateurs pendant la durée légale de conservation des factures clients.
+        L'Utilisateur, dans le délai imparti, peut obtenir à tout moment sur le site Swappist un état complet des Opérations de Paiement qu'il a réalisées.<br>
+<div><?php include('includes/footer.php'); ?></div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+</body>
 </html>
