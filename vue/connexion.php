ee1966850d9d57b808589f14129da3e774f7fad0
Unknown <hmscompagnie@gmail.com>
2018-01-16 11:37:21 +0100

    Style apres bootstrap

--- vue/connexion.php
+++ vue/connexion.php
@@ -3,8 +3,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="../includes/styles/style.css">
     <link rel="stylesheet" href="../includes/styles/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../includes/styles/style.css">
     <link rel="stylesheet" type="text/css" href="../includes/styles/footer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Swapp - Connexion</title>
