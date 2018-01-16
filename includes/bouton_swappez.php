12e1152aa64b03b45961e218518ed4f17f4d57a2
Unknown <hmscompagnie@gmail.com>
2018-01-16 12:04:22 +0100

    avec l'index encore mieux que meilleur

--- includes/bouton_swappez.php
+++ includes/bouton_swappez.php
@@ -1,7 +1,9 @@
 <body>
-    <a type="button" class="btn btn-default" href="#"> Prêt ! Feu ! <br />Swappez !</a>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap.min.js"></script>
+<a type="button" id="swappez" class="btn btn-default" href="#" style="height:85px;width:300px;background-color:deepskyblue;color:white;min-font-size:30%"> Prêt, feu... <br />Swappez !</a>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="bootstrap.min.js"></script>
+
 </body>
 </html>
