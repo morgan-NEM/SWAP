18021079e3a39b4df4d1f6a479e47d3bffeb2e0e
Unknown <hmscompagnie@gmail.com>
2018-01-16 09:07:12 +0100

    accueil Emma

--- vue/index.php
+++ vue/index.php
@@ -10,25 +10,290 @@
 </head>
 <body>
 <div><?php include('../includes/banner.php'); ?></div>
-<div><?php include('../includes/message_accueil.php'); ?></div>
 
-<section>   
+
+<section>
     <div class="row">
-    <div class="col-sm-1"></div>
-            <div class="col-sm-4">
-                <?php include('../includes/der_articles.php'); ?>
+
+        <div class="col-sm-1"></div>
+
+        <span>
+
+
+
+     <div >
+
+        <h1 class="centrer" style="color:white">Bienvenue chez Swappist !</h1>
+
+        <h3 class="gras centrer">☆ Le meilleur site de troc ☆</h3>
+
+         <div class="container" style="margin-top:40px">
+
+            <div class="col-lg-6">
+
+              <img src="../images/accueil.png" height="500" width="500" alt="Accueil">
+
             </div>
-            <div class="col-sm-3"></div>
-            <div class="col-sm-4">
-                <?php include('../includes/bouton_swappez.php'); ?>
+
+            <div class="col-lg-6"">
+
+            <p class="centrer">
+
+                <h3 class="centrer">Comment Swapper...</h3><br>
+
+                <br>
+
+                <h4>Je propose un Swap :</h4>
+
+                <br>
+
+                <ul class="icon">
+
+                    <li>Je mets mon annonce en ligne.
+
+                        (pensez qu'une annonce avec photo est 10 fois plus consultée).</li>
+
+                    <li>Quand un Swapper me fait une demande je l'accepte si elle me convient.</li>
+
+                    <li>Une fois le Swap réalisé je reçois mes SouApp sur mon profil (si le troc
+
+                    s'est fait avec des SouApp) et je laisse une évaluation.</li>
+
+                </ul>
+
+                <br>
+
+                <p class="centrer">OU</p>
+
+                <br>
+
+             <h4>Je recherche un Swap :</h4><br>
+
+             <ul class="icon">
+
+                 <li>Je fais une recherche par catégorie ou sur la carte.</li>
+
+                 <li>Je peux réserver directement l'objet swappé, le négocier ou simplement envoyer un message.</li>
+
+                <li>Une fois le Swap réalisé je valide les SouApp à troquer (si le Swap s'est fait avec des SouApp)
+
+                    et je laisse une évaluation.</li>
+
+             </ul>
+
+             </p>
+
             </div>
+
+        </div>
+
     </div>
 
-    <div class="row">
-            <div class="col-sm-8"></div>
-            <div class="col-sm-4">
-                <?php include('../includes/avis.php'); ?>
+
+
+    <div class="container main-section" style="margin-bottom:40px">
+
+        <div class="row">
+
+            <div class="col-md-4 col-sm-4 col-xs-12 product">
+
+                <div class="row product-part">
+
+                    <div class="col-md-12 col-sm-12 colxs-12 img-section">
+
+                        <img src="https://s-i.huffpost.com/gen/2210434/images/n-MCDONALDS-CUP-SIZE-200x150.jpg" alt="Gobelet">
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-title">
+
+                        <h1>Gobelets McDo TBE</h1>
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-description">
+
+                        <p>Je troque ces magnifiks gobelets McDo en parfait état de marche. Ils ont très peu servi.
+
+                            L'un des deux fuit un peu mais cer pas grav, avec une rustine il a encore de nombreuses heures devant lui.
+
+                            Je Swappe contre un iPhone 11+ exclusivement. Pas sérieux savent s'tenir.
+
+                        </p>
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-cart">
+
+                        <div class="row">
+
+                            <div class="col-md-6 col-sm-12 col-xs-6">
+
+                                <p>5 SouApp</p>
+
+                            </div>
+
+                            <div class="col-md-6 col-sm-12 col-xs-6 text-right product-add-cart">
+
+                                <a href="#" class="btn btn-success">JE SWAPPE</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
             </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12">
+
+                <div class="row product-part">
+
+                    <div class="col-md-12 col-sm-12 colxs-12 img-section">
+
+                        <img src="http://newcarsmodelupdate.net/wp-content/uploads/2018/01/2018-ford-raptor-tire-size-best-of-img-5382-of-2018-ford-raptor-tire-size-200x150.jpg">
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-title">
+
+                        <h1>Twingo Tunée - série limitée</h1>
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-description">
+
+                        <p>J'échange à regret ma Twingo Kenzo 1998 tunée (123 456 789 km, CT OK, pneus neufs,
+
+                            peinture effet Velvet) contre ce que vous voulez, mon mari ne la supporte plus...
+
+                            Sinon je swappe mon mari contre prise en charge des frais de divorce.
+
+                        </p>
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-cart">
+
+                        <div class="row">
+
+                            <div class="col-md-6 col-sm-12 col-xs-6">
+
+                                <p>3 SouApp</p>
+
+                            </div>
+
+                            <div class="col-md-6 col-sm-12 col-xs-6 text-right product-add-cart">
+
+                                <a href="#" class="btn btn-success">JE SWAPPE</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-md-4 col-sm-4 col-xs-12">
+
+                <div class="row product-part">
+
+                    <div class="col-md-12 col-sm-12 colxs-12 img-section">
+
+                        <img src="https://nbk9training.com/wp-content/uploads/2015/06/small-dog-200x150.jpg">
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-title">
+
+                        <h1>Perdu ma Kitty</h1>
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-description">
+
+                        <p>J'ai perdu ma petite chienne Kitty sur le port de Marseilla l'autre jour.
+
+                            Je suis bien embêtée car je l'aimais bien quand même, elle me tenait chaud le soir
+
+                            devant la télé quand je regardais les Anges. Merci à toussssss. Kissssooooous les Swappers. Véronik ❤︎
+
+                    </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12 product-cart">
+
+                        <div class="row">
+
+                            <div class="col-md-6 col-sm-12 col-xs-6">
+
+                                <p>6 SouApp</p>
+
+                            </div>
+
+                            <div class="col-md-6 col-sm-12 col-xs-6 text-right product-add-cart">
+
+                                <a href="#" class="btn btn-success">JE SWAPPE</a>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    </span>
+
+    </div>
+
+    </div>
+
+    <div class="row" style="margin-bottom:15px">
+
+        <div class="col-sm-2"></div>
+
+        <div class="col-sm-2">
+
+            <?php include ('../includes/bouton_swappez.php'); ?>
+
+        </div>
+
+        <div class="col-sm-2"></div>
+
+        <div class="col-sm-4">
+
+            <div class="panel panel-default" id="slogan">
+
+                <div class="panel-body" style="font-size:20px">Swappez en mode Troc classique</div>
+
+            </div>
+
+            <div class="panel panel-default" id="slogan">
+
+                <div class="panel-body" style="font-size:20px">Swappez contre des SouApp</div>
+
+            </div>
+
+            <div class="panel panel-default" id="slogan">
+
+                <div class="panel-body" style="font-size:20px">Négociez le Swapp (objet + SouApp)</div>
+
+            </div>
+
+        </div>
+
+        <div class="col-sm-2"></div>
 </section>            
 <div><?php include('../includes/footer.php'); ?></div>
 
