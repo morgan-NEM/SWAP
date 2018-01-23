35c262bc2fbfa11206797854f5593847a890d29e
Unknown <hmscompagnie@gmail.com>
2018-01-23 09:57:10 +0100

    maj page porduits

--- vue/produits.php
+++ vue/produits.php
@@ -1,131 +1,260 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="../includes/styles/style.css">
-    <link rel="stylesheet" href="../includes/styles/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../includes/styles/footer.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Swapp - Catégories</title>
-</head>
-<body>
-
-<div id='wrap'>
-    <div class='container'>
-        <div class='row'>
-            <div class='tag-header'>#swaps</div>
-        </div>
-        <div class='row'>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://d19elpabv0ne56.cloudfront.net/1445207752/products-thumbnail/2000x2000/0/0/0/0/3/9/2/1/2/0/7/3921207-1.jpg'>
-                    </div>
-                    <div class='product-price'>200 ♨︎</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://velo-electrique.valdeloirevelo.fr/img/cms/montage-kit-electrique-blois-tours.jpg'>
-                    </div>
-                    <div class='product-price'>15 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://bestoutdoorpingpongtables.com/wp-content/uploads/2017/02/BOPPT-500M-2-1024x865.jpg'>
-                    </div>
-                    <div class='product-price'>234 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://tinhte.cdnforo.com/store/2012/06/2981373_DSC09047.jpg'>
-                    </div>
-                    <div class='product-price'>15 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://pmcdn.priceminister.com/photo/1004690915.jpg'>
-                    </div>
-                    <div class='product-price'>350 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://img.fr.clasf.com/2016/11/29/achetez-machine-laver-occasion-annonce-vente-nmes-30-wb154816-20161129101628.jpg'>
-                    </div>
-                    <div class='product-price'>30 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://i.pinimg.com/originals/b5/a7/9d/b5a79d3cc68d290648440baedb4d93e0.jpg'>
-                    </div>
-                    <div class='product-price'>50 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='http://www.jenbodyart.com/wp-content/uploads/2016/12/transfert-dent-dents-dentier-monstre-halloween-effetsspeciaux-effets-speciaux-mignon-dessin-jen-bodyart-2-1000x1000.jpg'>
-                    </div>
-                    <div class='product-price'>100 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://media.paruvendu.fr/image/vetements-enfant-garcon/WB15/5/6/WB155609349_1.jpeg'>
-                    </div>
-                    <div class='product-price'>8 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='http://i.dailymail.co.uk/i/pix/2015/11/05/10/2E2115E000000578-3304929-image-a-59_1446719557565.jpg'>
-                    </div>
-                    <div class='product-price'>10 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='https://i.ytimg.com/vi/BAWXA8Mau6k/maxresdefault.jpg'>
-                    </div>
-                    <div class='product-price'>20 ♨</div>
-                </div>
-            </div>
-            <div class='col-xs-12 col-md-6 col-lg-4'>
-                <div class='product-box'>
-                    <div class='product'>
-                        <img class='product-img' src='http://blog.colinandjeanne.org/wp-content/uploads/2012/05/DSC_0131.jpg'>
-                    </div>
-                    <div class='product-price'>2 ♨</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-<div><?php include('../includes/banner.php'); ?></div>
-<h1 class="centrer">Catégories</h1>
-<div><?php include('../includes/liste_categories.php'); ?></div>
-
-<div><?php include('../includes/footer.php'); ?></div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap.min.js"></script>
-</body>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="../includes/styles/style.css">
+
+    <link rel="stylesheet" href="../includes/styles/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="../includes/styles/footer.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Swapp - Produits</title>
+
+</head>
+
+<body>
+
+<div><?php include('../includes/banner.php'); ?></div>
+
+<h1 class="centrer">Produits</h1>
+
+
+
+<div id='wrap'>
+
+    <div class='container'>
+
+        <div class='row'>
+
+            <div class='tag-header'>#swaps</div>
+
+        </div>
+
+        <div class='row'>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://d19elpabv0ne56.cloudfront.net/1445207752/products-thumbnail/2000x2000/0/0/0/0/3/9/2/1/2/0/7/3921207-1.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>200 ♨︎</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://velo-electrique.valdeloirevelo.fr/img/cms/montage-kit-electrique-blois-tours.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>15 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://bestoutdoorpingpongtables.com/wp-content/uploads/2017/02/BOPPT-500M-2-1024x865.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>234 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://tinhte.cdnforo.com/store/2012/06/2981373_DSC09047.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>15 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://pmcdn.priceminister.com/photo/1004690915.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>350 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://img.fr.clasf.com/2016/11/29/achetez-machine-laver-occasion-annonce-vente-nmes-30-wb154816-20161129101628.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>30 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://i.pinimg.com/originals/b5/a7/9d/b5a79d3cc68d290648440baedb4d93e0.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>50 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='http://www.jenbodyart.com/wp-content/uploads/2016/12/transfert-dent-dents-dentier-monstre-halloween-effetsspeciaux-effets-speciaux-mignon-dessin-jen-bodyart-2-1000x1000.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>100 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://media.paruvendu.fr/image/vetements-enfant-garcon/WB15/5/6/WB155609349_1.jpeg'>
+
+                    </div>
+
+                    <div class='product-price'>8 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='http://i.dailymail.co.uk/i/pix/2015/11/05/10/2E2115E000000578-3304929-image-a-59_1446719557565.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>10 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='https://i.ytimg.com/vi/BAWXA8Mau6k/maxresdefault.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>20 ♨</div>
+
+                </div>
+
+            </div>
+
+            <div class='col-xs-12 col-md-6 col-lg-4'>
+
+                <div class='product-box'>
+
+                    <div class='product'>
+
+                        <img class='product-img' src='http://blog.colinandjeanne.org/wp-content/uploads/2012/05/DSC_0131.jpg'>
+
+                    </div>
+
+                    <div class='product-price'>2 ♨</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div><?php include('../includes/footer.php'); ?></div>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <script src="bootstrap.min.js"></script>
+
+</body>
+
 </html>
